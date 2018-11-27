6e18966d8aea76e617feb67944d18f13dd9ce28b
Gaara <759499745@qq.com>
2018-11-27 19:44:48 +0800

    add cas client

--- src/main/java/com/gaara/mp3/Mp3Application.java
+++ src/main/java/com/gaara/mp3/Mp3Application.java
@@ -1,9 +1,11 @@
 package com.gaara.mp3;
 
+import net.unicon.cas.client.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCasClient
 public class Mp3Application {
 
 	public static void main(String[] args) {
