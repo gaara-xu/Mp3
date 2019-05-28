8a178840fda56dd652475a45ebfb7ddb8ab4357f
Gaara <759499745@qq.com>
2019-05-28 08:52:33 +0800

    完善

--- shell.sh
+++ shell.sh
@@ -1,3 +1,9 @@
+port=9000
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+if [  -n  "$pid"  ];  then
+    kill  -9  $pid;
+    echo " java.shell is killed  by" $pid;
+fi
 echo "" > nohup.out
 git pull origin shell;
 mvn clean package;
